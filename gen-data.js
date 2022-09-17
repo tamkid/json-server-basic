c7eb9d3473942a4383460ff7e3968ea4626d5762
Tom Nguyen <tamkid2408@gmail.com>
2022-09-17 10:27:20 +0700

    add more books data

--- gen-data.js
+++ gen-data.js
@@ -42,15 +42,50 @@ const randomProducts = (categories, n) => {
     return productList;
 }
 
+const initBooks = () => {
+    const books = [
+        {
+            id: casual.uuid,
+            name: "Learning Angular",
+            description: "A no-nonsense beginner's guide to building web applications with Angular 10 and TypeScript, 3rd Edition",
+            imageUrl: "https://m.media-amazon.com/images/P/1839210664.01._SCLZZZZZZZ_SX500_.jpg",
+            author: "Aristeidis Bampakos and Pablo Deeleman",
+            price: 36.99,
+            isAvailable: true
+        },
+        {
+          id: casual.uuid,
+          name: "ASP.NET Core 5 and Angular",
+          description: "Full-stack web development with .NET 5 and Angular 11, 4th Edition",
+          imageUrl: "https://m.media-amazon.com/images/P/1800560338.01._SCLZZZZZZZ_SX500_.jpg",
+          author: "Valerio De Sanctis",
+          price: 37.99,
+          isAvailable: true
+        },
+        {
+          id: casual.uuid,
+          name: "Angular Development with TypeScript",
+          description: "Angular Development with TypeScript, Second Edition is an intermediate-level tutorial that introduces Angular and TypeScript to developers comfortable with building web applications using other frameworks and tools.",
+          imageUrl: "https://images-na.ssl-images-amazon.com/images/I/41lozQ0E9ML._SX397_BO1,204,203,200_.jpg",
+          author: "Yakov Fain and Anton Moiseev",
+          price: 32.49,
+          isAvailable: true
+        }
+    ];
+    return books;
+}
+
 (() => {
     // random data
     const categoryList = randomCategories(4);
     const productList = randomProducts(categoryList, 5);
+    const bookList = initBooks();
 
     // prepare data object
     const objDb = {
         categories: categoryList,
         products: productList,
+        books: bookList,
         profile: {
             name: "Tom Nguyen"
         }
